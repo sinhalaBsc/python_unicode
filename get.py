69ebcac85d1bcda959542a4d5951d895f6333143
samadhi laksahan <35246020+sinhalaBsc@users.noreply.github.com>
2018-12-30 22:10:44 +0300

    Update get.py

--- get.py
+++ get.py
@@ -4,7 +4,6 @@ import time
 
 for i in range(1):
     response =requests.get('https://www.maduraonline.com/?find=cat')
-    # type(response) - it is a class >> <class 'request.model.Response'>
     print(response.text.encode('utf-8'))
     time.sleep(1)
 
